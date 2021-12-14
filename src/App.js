1e8f01a3b482d6e30870d8afbdba0373b4d6bb1a
leo-MARIE <leomarie91@gmail.com>
2021-12-13 20:34:20 -0500

    test cell

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import './App.css';
 import Game from './components/Game.js';
 import Win from './components/Win.js';
 
-let track = new Audio("https://raw.githubusercontent.com/leo-marie/cadeau-ali/main/public/samples/track.mp3");
-// let track = new Audio("https://raw.githubusercontent.com/leo-marie/Sequencer-Portal---frontend/main/public/samples/tekno02.mp3");
+// let track = new Audio("https://github.com/leo-marie/cadeau-ali/blob/5fc5540b792a15e68b04d994b11308dc03ea23d1/public/track.mp3");
+let track = new Audio("https://raw.githubusercontent.com/leo-marie/cadeau-ali/main/public/track.mp3")
 
 function App() {
 
   const [view, setView] = useState("game");
 
   const handleWinClick = () => {
-    setView("win");
+    setView("load");
+    setTimeout(() => {
+      setView("win")
+    }, 3000);
   }
 
   // RENDER
   const renderContent = () => {
     if (view == "game") {
-      track.play();
       return <Game handleWinClick={handleWinClick} />;
     }
     else if(view == "win") {
-      return <Win />;
-    }
+        return <Win />; 
+      };
+  
   } // rendercontent
 
   const testClick = () => {
@@ -31,11 +34,21 @@ function App() {
     track.play();
   }
 
+  useEffect(() => {
+    if(view == "load") {
+      track.pause();
+      track.currentTime = 0;
+      track.play();
+      return () => {
+
+      }
+    }
+  }, [view])
+
 
   return (
     <div>
       <div id="content">
-        <button onClick={testClick}>TEST</button>
         {renderContent()}
       </div>
     </div>
