8453b9c963d425fb15c49bc3b58e0674f6078097
leo-MARIE <leomarie91@gmail.com>
2021-12-13 19:34:29 -0500

    responsive

--- src/components/Win.js
+++ src/components/Win.js
@@ -6,11 +6,9 @@ const Win = () => {
     return (
 
         <div id="win-content">
-            <iframe width="420" height="315"
-            src="https://www.youtube.com/watch?v=zEZRKgFIkxc?autoplay=1">
-            </iframe>
+            <iframe width="260" height="180" src="https://www.youtube.com/embed/zEZRKgFIkxc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <img id="image-billets" src={billets}></img>
-            <div className="description">2 billets dans la fosse pour aller avec qui tu veux ! Par exemple moi !</div>
+            <div className="description2">2 billets dans la fosse pour aller avec qui tu veux ! Par exemple moi !</div>
         </div>
 
 
